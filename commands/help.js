e59aec4f3fe876d7d1416b35d3fa44d9fa5c5710
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-06-13 19:38:46 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports={
     run: async(client,message, args)=>{
         const Embed = new MessageEmbed()
         .setColor(`BLUE`)
-        .setTitle(`All SPEDBot Commands`)
+        .setTitle(`:clipboard: All SPEDBot Commands :clipboard:`)
         .setThumbnail('https://gyazo.com/c7234d523a8b1a82d90a12e2b9199052.png')
         .addField('😄 Fun Commands 😄', '`meme, topic, 8ball, giveaway, poll`')
         .addField('ℹ️ Info Comamnds ℹ️', '`ping, help, gameinfo, avatar`')
